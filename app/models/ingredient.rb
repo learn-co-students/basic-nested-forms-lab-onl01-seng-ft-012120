e8b463849982866ac076459ac48a749728b12e9c
cosmicChimp <ian.t.marrs@gmail.com>
2020-04-03 14:02:20 -0600

    Done.

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,3 +1,10 @@
 class Ingredient < ActiveRecord::Base
   belongs_to :recipe
+  def recipe_id=(id)
+    self.recipe = Recipe.find_or_create_by(id: self.recipe_id)
+  end
+
+  def recipe_id
+    self.recipe ? self.recipe_id : nil
+  end
 end
